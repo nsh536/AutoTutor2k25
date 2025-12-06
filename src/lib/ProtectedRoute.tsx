b17d214e829608ea0800ac1f4ff497d0c3f4a16f
nsh536 <narasimhagolla8844@gmail.com>
2025-12-06 15:38:06 +0530

    Create ProtectedRoute.tsx

---
+++ src/lib/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { supabase } from "../integrations/supabase";
+
+export default function ProtectedRoute({ children }: any) {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+  }, []);
+
+  if (user === null) return <p>Loading...</p>;
+
+  return user ? children : <Navigate to="/login" />;
+}
